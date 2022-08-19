32ad3938503eaa678361b72e7a9492d16df3a586
JangHoonSeok <orirori512@naver.com>
2022-08-19 14:14:30 +0900

    polymorphism.ts

---
+++ polymorphism.ts
@@ -0,0 +1,20 @@
+type Player<E> = {
+  name: string;
+  extraInfo: E;
+};
+type NicoExtra = {
+  favFood: string;
+};
+type NicoPlayer = Player<NicoExtra>;
+
+const nico: NicoPlayer = {
+  name: "nico",
+  extraInfo: {
+    favFood: "kimchi",
+  },
+};
+
+const lynn: Player<null> = {
+  name: "lynn",
+  extraInfo: null,
+};
