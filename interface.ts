e0393ee11cc0ee8e819f7fdb3dff5f49b1408791
JangHoonSeok <orirori512@naver.com>
2022-08-20 17:38:23 +0900

    interface

---
+++ interface.ts
@@ -0,0 +1,23 @@
+interface User {
+  firstName: string;
+  lastName: string;
+  sayHi(name: string): string;
+  fullName(): string;
+}
+interface Human {
+  health: number;
+}
+
+class Player2 implements User, Human {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public health: number
+  ) {}
+  fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+  sayHi(name: string): string {
+    return `Hello ${name} My name is ${this.fullName()}`;
+  }
+}
